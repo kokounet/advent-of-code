406a868489eef0cdd04d6de0938546d4ada89c6d
Chris Janaqi <chris.janaqi@gmail.com>
2022-12-03 09:48:22 +0100

    update cargo config

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,11 @@
 use anyhow::Error;
 use std::fs;
 
-fn part1(elfs: &Vec<Vec<u32>>) -> u32 {
+fn part1(elfs: &[Vec<u32>]) -> u32 {
     elfs.iter().map(|snacks| snacks.iter().sum()).max().unwrap()
 }
 
-fn part2(elfs: &Vec<Vec<u32>>) -> u32 {
+fn part2(elfs: &[Vec<u32>]) -> u32 {
     let mut total: Vec<_> = elfs.iter().map(|snacks| snacks.iter().sum()).collect();
     total.sort();
     total.iter().rev().take(3).sum()
@@ -13,7 +13,7 @@ fn part2(elfs: &Vec<Vec<u32>>) -> u32 {
 
 fn main() -> Result<(), Error> {
     let content = fs::read_to_string("day01/input.txt")?;
-    let elfs: Vec<Vec<u32>> = content.split("\n").fold(vec![vec![]], |mut acc, curr| {
+    let elfs: Vec<Vec<u32>> = content.split('\n').fold(vec![vec![]], |mut acc, curr| {
         match curr.trim().parse() {
             Ok(calories) => acc.last_mut().unwrap().push(calories),
             Err(_) => acc.push(Vec::new()),
