54d940dc6b42f11c016778bc2d4a7d20dc354e50
Chris Janaqi <chris.janaqi@gmail.com>
2022-12-07 20:22:06 +0100

    Day 07 solution in python

---
+++ day07/main.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+
+
+def main():
+    available = 70_000_000
+    update = 30_000_000
+    with Path("day07/input.txt").open() as file:
+        lines = [l.strip().split() for l in file]
+
+    files = {}
+    dirs = defaultdict(lambda: 0)
+    cwd = ["/"]
+    for line in lines:
+        match line:
+            case ["$", "cd", ".."]:
+                cwd.pop()
+            case ["$", "cd", "/"]:
+                cwd = ["/"]
+            case ["$", "cd", dir]:
+                cwd.append(dir)
+            case ["$", cmd]:
+                pass
+            case [ssize, name] if ssize not in ["$", "dir"]:
+                size = int(ssize)
+                available -= size
+                files[tuple([*cwd, name])] = size
+                for i in range(len(cwd)):
+                    dirs[tuple(cwd[: i + 1])] += size
+
+    print(sum(filter(lambda size: size < 100000, dirs.values())))
+    print(min(filter(lambda size: (available + size) > update, dirs.values())))
+
+
+if __name__ == "__main__":
+    main()
