df2f809b87431bfbff2147731f14cf99db09bf3b
Chris Janaqi <chris.janaqi@gmail.com>
2022-12-19 12:45:22 +0100

    Day 18 skeleton

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use block_mesh::{visible_block_faces, Voxel, VoxelVisibility};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Block {
+    Empty,
+    Lava,
+}
+
+fn main() -> Result<()> {
+    let content = std::fs::read_to_string("day18/example.txt")?;
+
+    Ok(())
+}
+
+impl Voxel for Block {
+    fn get_visibility(&self) -> block_mesh::VoxelVisibility {
+        match self {
+            Self::Empty => VoxelVisibility::Empty,
+            _ => VoxelVisibility::Opaque,
+        }
+    }
 }
