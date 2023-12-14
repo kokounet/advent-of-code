73dc431f03510da46691f93b6592b0e0f6cf4230
Chris Janaqi <chris.janaqi@xlnaudio.com>
2023-12-14 12:55:28 +0100

    Day 09 and 11 solutions

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -2,20 +2,21 @@ use anyhow::Error;
 use std::fs;
 
 fn main() -> Result<(), Error> {
-    let content = fs::read_to_string("day09/example.txt")?;
+    let content = fs::read_to_string("day09/input.txt")?;
     let histories: Vec<_> = content
         .lines()
         .map(|line| {
             line.split_whitespace()
-                .map(|num| num.parse::<u32>())
+                .map(|num| num.parse::<i32>())
                 .collect::<Result<Vec<_>, _>>()
         })
         .collect::<Result<Vec<_>, _>>()?;
     println!("{}", part1(&histories));
+    println!("{}", part2(&histories));
     Ok(())
 }
 
-fn part1(histories: &[Vec<u32>]) -> u32 {
+fn part1(histories: &[Vec<i32>]) -> i32 {
     let mut sum = 0;
     for history in histories {
         let mut diffs = vec![history.clone()];
@@ -30,7 +31,26 @@ fn part1(histories: &[Vec<u32>]) -> u32 {
         for diff in diffs.iter().rev() {
             prediction += diff.last().unwrap();
         }
-        println!("{prediction}");
+        sum += prediction;
+    }
+    sum
+}
+
+fn part2(histories: &[Vec<i32>]) -> i32 {
+    let mut sum = 0;
+    for history in histories {
+        let mut diffs = vec![history.clone()];
+        while let Some(prev) = diffs.last() {
+            let new: Vec<_> = prev.windows(2).map(|win| win[1] - win[0]).collect();
+            if new.iter().all(|num| *num == 0) {
+                break;
+            }
+            diffs.push(new)
+        }
+        let mut prediction = 0;
+        for diff in diffs.iter().rev() {
+            prediction = diff.first().unwrap() - prediction;
+        }
         sum += prediction;
     }
     sum
--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -1,78 +1,69 @@
 use anyhow::Error;
+use std::collections::BTreeSet;
 use std::fs;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum Cell {
-    Empty,
-    Galaxy,
-}
-
 fn main() -> Result<(), Error> {
-    let content = fs::read_to_string("day11/example.txt")?;
-    let map: Vec<Vec<_>> = content
+    let content = fs::read_to_string("day11/input.txt")?;
+    let galaxies: BTreeSet<_> = content
         .lines()
-        .map(|line| {
+        .enumerate()
+        .flat_map(|(row, line)| {
             line.chars()
-                .map(|c| match c {
-                    '.' => Cell::Empty,
-                    '#' => Cell::Galaxy,
+                .enumerate()
+                .filter_map(move |(col, c)| match c {
+                    '.' => None,
+                    '#' => Some((row, col)),
                     _ => unreachable!(),
                 })
-                .collect()
         })
         .collect();
-    let expanded = expand(&map);
-    display_map(&map);
-    display_map(&expanded);
+    println!("{}", part1(&galaxies));
+    println!("{}", part2(&galaxies));
     Ok(())
 }
 
-fn expand(map: &[Vec<Cell>]) -> Vec<Vec<Cell>> {
-    let mut new = map.to_vec();
-    let rows = map.len();
-    let cols = map[0].len();
-    let empty_rows: Vec<_> = map
-        .iter()
-        .enumerate()
-        .filter_map(|(i, row)| {
-            row.iter()
-                .all(|cell| matches!(cell, Cell::Empty))
-                .then_some(i)
-        })
-        .collect();
-    let empty_cols: Vec<_> = (0..cols)
-        .filter_map(|col| {
-            map.iter()
-                .all(|row| matches!(row[col], Cell::Empty))
-                .then_some(col)
-        })
-        .collect();
-    for row in new.iter_mut() {
-        for empty_col in empty_cols.iter().rev() {
-            row.insert(*empty_col, Cell::Empty);
+fn part1(galaxies: &BTreeSet<(usize, usize)>) -> usize {
+    let expanded = expand(galaxies, 2);
+    let mut sum = 0;
+    for (row1, col1) in expanded.iter() {
+        for (row2, col2) in expanded.range((*row1, *col1)..) {
+            if (row1 == row2) && (col1 == col2) {
+                continue;
+            }
+            let dist = row2.abs_diff(*row1) + col2.abs_diff(*col1);
+            sum += dist;
         }
     }
-    let new_cols = new[0].len();
-    let empty = vec![Cell::Empty; new_cols];
-    for empty_row in empty_rows.iter().rev() {
-        new.insert(*empty_row, empty.clone());
-    }
-    let new_rows = new.len();
-    println!("({rows}, {cols})->({new_rows}, {new_cols})");
-    new
+    sum
 }
 
-fn display_map(map: &[Vec<Cell>]) {
-    for row in map.iter() {
-        for cell in row {
-            print!(
-                "{}",
-                match cell {
-                    Cell::Empty => '.',
-                    Cell::Galaxy => '#',
-                }
-            );
+fn part2(galaxies: &BTreeSet<(usize, usize)>) -> usize {
+    let expanded = expand(galaxies, 1_000_000);
+    let mut sum = 0;
+    for (row1, col1) in expanded.iter() {
+        for (row2, col2) in expanded.range((*row1, *col1)..) {
+            if (row1 == row2) && (col1 == col2) {
+                continue;
+            }
+            let dist = row2.abs_diff(*row1) + col2.abs_diff(*col1);
+            sum += dist;
         }
-        println!("");
     }
+    sum
+}
+
+fn expand(galaxies: &BTreeSet<(usize, usize)>, factor: usize) -> BTreeSet<(usize, usize)> {
+    let (rows, cols): (BTreeSet<_>, BTreeSet<_>) = galaxies.iter().cloned().unzip();
+    let all_rows: BTreeSet<_> = (0..=*rows.last().unwrap()).collect();
+    let all_cols: BTreeSet<_> = (0..=*cols.last().unwrap()).collect();
+    let empty_rows: BTreeSet<_> = all_rows.difference(&rows).cloned().collect();
+    let empty_cols: BTreeSet<_> = all_cols.difference(&cols).cloned().collect();
+    galaxies
+        .iter()
+        .map(|(row, col)| {
+            let rexpand = empty_rows.range(..row).count();
+            let cexpand = empty_cols.range(..col).count();
+            (row + rexpand * (factor - 1), col + cexpand * (factor - 1))
+        })
+        .collect()
 }
