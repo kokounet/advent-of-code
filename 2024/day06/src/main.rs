b4ab218410bdfec2e7e6ee853a332c17779f2502
Chris Janaqi <chris.janaqi@xlnaudio.com>
2024-12-09 17:48:31 +0100

    2024: Day 06 p2 solution in rust

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -1,9 +1,8 @@
 use std::collections::HashSet;
 
-use anyhow::Result;
 use glam::IVec2;
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     let content = std::fs::read_to_string("day06/input.txt")?;
     let lines = content.lines().collect::<Vec<_>>();
     let (w, h) = (lines[0].len() as i32, lines.len() as i32);
@@ -31,10 +30,11 @@ fn main() -> Result<()> {
             },
         );
     println!("{}", part1(start, &grid));
+    println!("{}", part2(start, grid));
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Grid {
     pub dims: IVec2,
     pub obstacles: HashSet<IVec2>,
@@ -64,6 +64,31 @@ impl Grid {
         }
         Ok(new)
     }
+
+    pub fn visit(&self, start: IVec2) -> Result<HashSet<(IVec2, IVec2)>, HashSet<(IVec2, IVec2)>> {
+        let dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)].map(IVec2::from);
+        let mut visited = HashSet::new();
+        let mut curr = start;
+        for dir in dirs.into_iter().cycle() {
+            loop {
+                if !visited.insert((curr, dir)) {
+                    // hit a cycle if we're at the same position as previously
+                    // with the same direction
+                    return Err(visited);
+                }
+                match self.walk(curr, dir) {
+                    Ok(pos) => {
+                        curr = pos;
+                    }
+                    Err(Hit::Obstable) => {
+                        break; // change direction
+                    }
+                    Err(Hit::OutOfBound) => return Ok(visited),
+                }
+            }
+        }
+        unreachable!("somehow broke out of an infinite loop")
+    }
 }
 
 enum Hit {
@@ -72,22 +97,29 @@ enum Hit {
 }
 
 fn part1(start: IVec2, grid: &Grid) -> i32 {
-    let dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)].map(IVec2::from);
-    let mut visited = HashSet::new();
-    let mut curr = start;
-    for dir in dirs.into_iter().cycle() {
-        loop {
-            visited.insert(curr);
-            match grid.walk(curr, dir) {
-                Ok(pos) => {
-                    curr = pos;
-                }
-                Err(Hit::Obstable) => {
-                    break; // change direction
-                }
-                Err(Hit::OutOfBound) => return visited.len() as i32,
-            }
-        }
-    }
-    unreachable!("somehow broke out of an infinite loop")
+    grid.visit(start)
+        .unwrap_or_else(|e| e)
+        .into_iter()
+        .map(|(pos, _)| pos)
+        .collect::<HashSet<_>>() // removes duplicates pos
+        .len() as i32
+}
+
+fn part2(start: IVec2, mut grid: Grid) -> i32 {
+    let mut points: HashSet<_> = grid
+        .visit(start)
+        .unwrap()
+        .into_iter()
+        .map(|(pos, _)| pos)
+        .collect();
+    points.remove(&start); // cant place an obstacle at the start
+    points
+        .into_iter()
+        .filter(|&point| {
+            grid.obstacles.insert(point);
+            let visited = grid.visit(start);
+            grid.obstacles.remove(&point);
+            visited.is_err() // detected a cycle
+        })
+        .count() as i32
 }
