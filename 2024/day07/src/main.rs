2373a56e17559907ec9d6eaedbc2fa150a7b9347
Chris Janaqi <chris.janaqi@xlnaudio.com>
2024-12-10 11:23:43 +0100

    2024: Day 07: much more efficient solutions using recursion and pruning

--- 2024/day07/src/main.rs
+++ 2024/day07/src/main.rs
@@ -25,48 +25,87 @@ fn main() -> Result<()> {
 fn part1(equations: &[(i64, Vec<i64>)]) -> i64 {
     equations
         .iter()
-        .filter_map(|(expected, operands)| {
-            let num_ops = operands.len() - 1;
-            (0..(1 << num_ops))
-                .map(|bits| {
-                    let mut it = operands.iter().cloned();
-                    let mut res = it.next().unwrap();
-                    for (i, e) in it.enumerate() {
-                        if (bits >> i) & 1 == 1 {
-                            res *= e;
-                        } else {
-                            res += e;
-                        }
-                    }
-                    res
-                })
-                .find(|res| res == expected)
-        })
+        .filter_map(|(expected, operands)| solve1(*expected, operands).then_some(expected))
         .sum()
+}
+
+fn solve1(expected: i64, operands: &[i64]) -> bool {
+    assert!(operands.len() != 0);
+    if operands.len() == 1 {
+        return operands[0] == expected;
+    }
+    let last = operands.last().unwrap();
+    let rest = &operands[..operands.len() - 1];
+    let mut result = false;
+    if expected % last == 0 {
+        // we can't exclude mul for this operand
+        result = solve1(expected / last, rest)
+    }
+    result || solve1(expected - last, rest)
 }
 
 fn part2(equations: &[(i64, Vec<i64>)]) -> i64 {
     equations
         .iter()
-        .filter_map(|(expected, operands)| {
-            let num_ops = operands.len() - 1;
-            (0..(3i64.pow(num_ops as u32)))
-                .map(|mask| {
-                    let mut it = operands.iter().cloned();
-                    let mut res = it.next().unwrap();
-                    for (i, e) in it.enumerate() {
-                        let flag = (mask / 3i64.pow(i as u32)) % 3;
-                        if flag == 0 {
-                            res += e;
-                        } else if flag == 1 {
-                            res *= e;
-                        } else if flag == 2 {
-                            res = format!("{res}{e}").parse().unwrap(); // yummy
-                        }
-                    }
-                    res
-                })
-                .find(|res| res == expected)
-        })
+        .filter_map(|(expected, operands)| solve2(*expected, operands).then_some(expected))
         .sum()
+}
+
+fn solve2(expected: i64, operands: &[i64]) -> bool {
+    assert!(operands.len() != 0);
+    let last = *operands.last().unwrap();
+    let rest = &operands[..operands.len() - 1];
+    if rest.is_empty() {
+        return expected == last;
+    }
+
+    let mut solvable = false;
+    if let Some(new) = remove_suffix(expected, last) {
+        // we can't exclude concat
+        solvable = solvable || solve2(new, rest);
+    }
+    if expected % last == 0 {
+        // we can't exclude mul
+        solvable = solvable || solve2(expected / last, rest);
+    }
+    solvable = solvable || solve2(expected - last, rest);
+    solvable
+}
+
+fn remove_suffix(num: i64, suffix: i64) -> Option<i64> {
+    if suffix == 0 {
+        return (num.rem_euclid(10) == 0).then_some(num.div_euclid(10));
+    }
+    let digits = suffix.ilog10() + 1;
+    let divisor = 10i64.pow(digits);
+    let dividend = num - suffix;
+    (dividend.rem_euclid(divisor) == 0).then_some(dividend.div_euclid(divisor))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::remove_suffix;
+
+    #[test]
+    fn test_remove_suffix() {
+        let num = 12345;
+        assert_eq!(remove_suffix(num, 5), Some(1234));
+        assert_eq!(remove_suffix(num, 45), Some(123));
+        assert_eq!(remove_suffix(num, 345), Some(12));
+        assert_eq!(remove_suffix(num, 2345), Some(1));
+        assert_eq!(remove_suffix(num, 12345), Some(0));
+
+        assert_eq!(remove_suffix(num, 22345), None);
+
+        assert_eq!(remove_suffix(num, 0), None);
+        assert_eq!(remove_suffix(num, 1), None);
+        assert_eq!(remove_suffix(num, 2), None);
+        assert_eq!(remove_suffix(num, 3), None);
+        assert_eq!(remove_suffix(num, 4), None);
+
+        assert_eq!(remove_suffix(num, 6), None);
+        assert_eq!(remove_suffix(num, 7), None);
+        assert_eq!(remove_suffix(num, 8), None);
+        assert_eq!(remove_suffix(num, 9), None);
+    }
 }
