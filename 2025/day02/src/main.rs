f627259447368654a526c3c60638270a299696c9
Chris Janaqi <chris.janaqi@xlnaudio.com>
2025-12-02 18:31:37 +0100

    2025: Day 02: format

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,40 +1,53 @@
-use std::{fs, ops::{RangeInclusive}};
+use std::{fs, ops::RangeInclusive};
 
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
 
 fn main() -> Result<()> {
     let content = fs::read_to_string("day02/input.txt")?;
-    let ranges = content.split(",").map(|range| {
-        let (low, high) = range.split_once("-").ok_or(anyhow!("unexpected range: {}", range))?;
-        let low = low.parse::<u64>()?;
-        let high = high.parse::<u64>()?;
-        Ok(low..=high)
-    }).collect::<Result<Vec<_>>>()?;
+    let ranges = content
+        .split(",")
+        .map(|range| {
+            let (low, high) = range
+                .split_once("-")
+                .ok_or(anyhow!("unexpected range: {}", range))?;
+            let low = low.parse::<u64>()?;
+            let high = high.parse::<u64>()?;
+            Ok(low..=high)
+        })
+        .collect::<Result<Vec<_>>>()?;
     println!("{}", part1(&ranges));
     println!("{}", part2(&ranges));
     Ok(())
 }
 
 fn part1(ranges: &[RangeInclusive<u64>]) -> u64 {
-    ranges.iter().flat_map(|range| {
-        range.clone().filter_map(|id| {
-            let digits = id.to_string();
-            let len = digits.len();
-            if len & 0x1 == 1 { return None; } // len is odd, can't be invalid
-            let mid = digits.len() / 2;
-            (digits[0..mid] == digits[mid..]).then_some(id)
+    ranges
+        .iter()
+        .flat_map(|range| {
+            range.clone().filter_map(|id| {
+                let digits = id.to_string();
+                let len = digits.len();
+                if len & 0x1 == 1 {
+                    return None;
+                } // len is odd, can't be invalid
+                let mid = digits.len() / 2;
+                (digits[0..mid] == digits[mid..]).then_some(id)
+            })
         })
-    }).sum()
+        .sum()
 }
 
 fn part2(ranges: &[RangeInclusive<u64>]) -> u64 {
-    ranges.iter().flat_map(|range| {
-        range.clone().filter_map(|id| {
-            let digits = id.to_string();
-            let len = digits.len();
-            (1..=len/2).any(|l| check(l, &digits)).then_some(id)
+    ranges
+        .iter()
+        .flat_map(|range| {
+            range.clone().filter_map(|id| {
+                let digits = id.to_string();
+                let len = digits.len();
+                (1..=len / 2).any(|l| check(l, &digits)).then_some(id)
+            })
         })
-    }).sum()
+        .sum()
 }
 
 fn check(k: usize, s: &str) -> bool {
@@ -46,5 +59,5 @@ fn check(k: usize, s: &str) -> bool {
     // n * k = s.len()
     let n = s.len() / k;
     let prefix = &s[0..k];
-    (1..n).all(|i| &s[k*i..k*(i+1)] == prefix)
+    (1..n).all(|i| &s[k * i..k * (i + 1)] == prefix)
 }
