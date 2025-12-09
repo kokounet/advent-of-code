7bff9cac448fb738f74a1734d3f46bc69a8099dd
Chris Janaqi <chris.janaqi@xlnaudio.com>
2025-12-09 09:59:00 +0100

    2025: Day 08: use sorted vec instead of BTreeSet for dist matrix
    The vec version 1.5x faster than the BTreeSet one, sad

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -25,35 +25,26 @@ fn dist2(a: &[i64; 3], b: &[i64; 3]) -> i64 {
     (a[0] - b[0]).pow(2) + (a[1] - b[1]).pow(2) + (a[2] - b[2]).pow(2)
 }
 
-fn dist_matrix(coords: &BTreeSet<[i64; 3]>) -> BTreeSet<(i64, [i64; 3], [i64; 3])> {
-    coords
+fn dist_matrix(coords: &BTreeSet<[i64; 3]>) -> Vec<([i64; 3], [i64; 3])> {
+    let mut mat: Vec<_> = coords
         .iter()
         .combinations(2)
-        .map(|points| {
-            let p1 = points[0].clone();
-            let p2 = points[1].clone();
-            let d = dist2(&p1, &p2);
-            (d, p1, p2)
-        })
-        .collect()
+        .map(|points| (*points[0], *points[1]))
+        .collect();
+    mat.sort_by(|(a1, a2), (b1, b2)| dist2(a1, a2).cmp(&dist2(b1, b2)));
+    mat
 }
 
 fn part1(coords: &BTreeSet<[i64; 3]>) -> i64 {
-    let mut dists = dist_matrix(coords);
+    let dists = dist_matrix(coords);
     let mut circuits: Vec<HashSet<[i64; 3]>> = coords
         .iter()
         .map(|pos| HashSet::from_iter([*pos]))
         .collect();
-    let mut it = 0;
 
-    while let Some((_, p1, p2)) = dists.pop_first() {
-        if it == 1000 {
-            break;
-        }
-
-        it += 1;
-        let i1 = circuits.iter().position(|set| set.contains(&p1)).unwrap();
-        let i2 = circuits.iter().position(|set| set.contains(&p2)).unwrap();
+    for (p1, p2) in &dists[..1000] {
+        let i1 = circuits.iter().position(|set| set.contains(p1)).unwrap();
+        let i2 = circuits.iter().position(|set| set.contains(p2)).unwrap();
         if i1 == i2 {
             continue;
         }
@@ -67,15 +58,15 @@ fn part1(coords: &BTreeSet<[i64; 3]>) -> i64 {
 }
 
 fn part2(coords: &BTreeSet<[i64; 3]>) -> i64 {
-    let mut dists = dist_matrix(coords);
+    let dists = dist_matrix(coords);
     let mut circuits: Vec<HashSet<[i64; 3]>> = coords
         .iter()
         .map(|pos| HashSet::from_iter([*pos]))
         .collect();
 
-    while let Some((_, p1, p2)) = dists.pop_first() {
-        let i1 = circuits.iter().position(|set| set.contains(&p1)).unwrap();
-        let i2 = circuits.iter().position(|set| set.contains(&p2)).unwrap();
+    for (p1, p2) in &dists {
+        let i1 = circuits.iter().position(|set| set.contains(p1)).unwrap();
+        let i2 = circuits.iter().position(|set| set.contains(p2)).unwrap();
         if i1 == i2 {
             continue;
         }
